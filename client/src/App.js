5bd8d79c628e502e553f25960252bfcc5ab89a7a
Murat Karakurt <55550212+mrtkrkrt@users.noreply.github.com>
2022-04-19 16:48:14 +0300

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Choose from "./Components/Choose";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Choose />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
