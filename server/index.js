52b6096aaa0d9c655e0361bada9de631da218a00
Murat Karakurt <55550212+mrtkrkrt@users.noreply.github.com>
2022-05-01 15:38:27 +0300

    .

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/fullGraph", (req, res) => {
-    // TODO write post request for cypher queries you can drawing whole node
-})
-
 app.post("/oneNode", (req, res) => {
     // TODO write post request for cypher queries you can draw only one node
 })
